27210773b38f93a45e33022dadc431d10c2d59eb
Hlengiwe Confidence Cele <154695394+cele-hlengiwe@users.noreply.github.com>
2023-12-30 21:45:02 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,8 @@
+# Build python script into a stand-alone exe
+- uses: Nuitka/Nuitka-Action@main
+  with:
+    nuitka-version: main
+    script-name: hello_world.py
 
+C:\> hello_world.exe
+hello world!
